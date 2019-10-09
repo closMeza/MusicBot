44f72c5b3a71e72355b3bddc638094431c5c104c
cmeza77306 <31258439+cmeza77306@users.noreply.github.com>
2019-10-09 09:35:38 -0700

    Added ListBox
    I think in order to add the the upvote downvote portion we may have to select item in list box then have two checkboxes pop up one for upvote one for downvote and then return these values back to AI. That last portion maybe the most difficult

--- MusicBotProto.py
+++ MusicBotProto.py
@@ -12,48 +12,56 @@ class Application(tk.Frame):
     # this creates all the widgets for our GUI
     def create_widgets(self):
 
-        #value for row on grid
-        r = 0
+        # value for row on grid
+        #r = 0
         
-        #Title Label maybe convert to image
+        # Title Label maybe convert to image
         self.header = tk.Label(self,
                              text="Music Bot",
                              font = "28",
                              height=2)
-        self.header.grid(row = r)
+        self.header.grid(row = 0, column = 0)
        
-       #textEntry for user input
+       # textEntry for user input
         self.textEntry = tk.Entry(self,
                                  width=20)
-        self.textEntry.grid(row = r, column=0)
+        self.textEntry.grid(row = 1, column=0)
 
-        #Search Button
+        # Search Button
         self.search = tk.Button(self,
                                 text="SEARCH",
                                 command = self.get_list)
-        self.search.grid(row = r, column=1)
+        self.search.grid(row = 1, column=1)
+
+        # ListView 
+        self.list_view = tk.Listbox(self)
+        self.list_view.grid(row = 2, column = 0)
+
         
         #quit button
         self.quit = tk.Button(self, text= "Quit", 
                               fg="red", 
                               command = self.master.destroy)
-        self.quit.grid(row = r)
+        self.quit.grid(row = 3, column = 1)
 
     # this is where we need to map user input to spotify database
     # we need to create a pkg for our AI to proccess input and return
     # list of songs based on genre
     def get_list(self):
         input = self.textEntry.get()
+        self.list_view.insert(0, input)
         self.textEntry.delete(0, 'end')
-        print(input)
+
 
 def main():
     root = tk.Tk()
     app = Application(master=root)
     root.title("MusicBot")
+    root.geometry("300x300")
     app.mainloop()
 
 if __name__ == "__main__":
     main()
+
 
 
