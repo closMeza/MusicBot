5eb78fe754a6a859fa1599b4975b95c604320252
cmeza77306 <31258439+cmeza77306@users.noreply.github.com>
2019-10-10 16:33:57 -0700

    Added spotify connection
    We now can pull tracks using Spotify. However can only do 1 track at a time and need the tracks URI to display it properly on GUI/Console

--- MusicBotProto.py
+++ MusicBotProto.py
@@ -1,13 +1,22 @@
 import tkinter as tk
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+
+
 
 class Application(tk.Frame):
    
     # Constructor for our GUI
+
     def __init__(self, master = None):
         super().__init__(master)
         self.master = master
         self.pack()
         self.create_widgets()
+        self.cid = "89ba12dbb318412baa7c3f1c642d17d9"
+        self.secret = "47689f35aa354d0dbe06d1b89323561d"
+
 
     # this creates all the widgets for our GUI
     def create_widgets(self):
@@ -49,7 +58,19 @@ class Application(tk.Frame):
     # list of songs based on genre
     def get_list(self):
         input = self.textEntry.get()
-        self.list_view.insert(0, input)
+        
+        #this needs to be set in init of Application this is the authorizes connection to spotify.
+        client_credentials_manager = SpotifyClientCredentials(client_id=self.cid, client_secret=self.secret) 
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+        #we need this URI to search items in spotify database.
+        URI = 'spotify:track:5bJ1DrEM4hNCafcDd1oxHx'
+        result = sp.track(URI)
+
+        #places result of spotify search and places it in list and prints it onto console
+        self.list_view.insert(0, result['name'])
+        print(result['name'])
+        
         self.textEntry.delete(0, 'end')
 
 
